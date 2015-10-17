ff3d4e579aa8ff14cb7b0360e45d3657616d4edb
Joel Hobson <joel.hobsn@gmail.com>
2015-10-17 17:03:11 -0400

    OpenOutputDevice seems to work

--- src/device/win.jl
+++ src/device/win.jl
@@ -1,44 +1,7 @@
 # This file controls streaming midi data to a device on your system. Windows only, until someone wants to port it to other systems and
 # submit a pull request.
 
-const CALLBACK_NULL = uint32(0x00000000)
-
-immutable SZPName
-    c1::UInt8
-    c2::UInt8
-    c3::UInt8
-    c4::UInt8
-    c5::UInt8
-    c6::UInt8
-    c7::UInt8
-    c8::UInt8
-    c9::UInt8
-    c10::UInt8
-    c11::UInt8
-    c12::UInt8
-    c13::UInt8
-    c14::UInt8
-    c15::UInt8
-    c16::UInt8
-    c17::UInt8
-    c18::UInt8
-    c19::UInt8
-    c20::UInt8
-    c21::UInt8
-    c22::UInt8
-    c23::UInt8
-    c24::UInt8
-    c25::UInt8
-    c26::UInt8
-    c27::UInt8
-    c28::UInt8
-    c29::UInt8
-    c30::UInt8
-    c31::UInt8
-    c32::UInt8
-end
-
-tostring(x::SZPName) = bytestring(pointer(ASCIIString([x.(z) for z in 1:length(names(x))])))
+const CALLBACK_NULL = UInt32(0x00000000)
 
 type MidiOutCaps
     wMid::UInt16
@@ -51,7 +14,6 @@ type MidiOutCaps
     wChannelMask::UInt16
     dwSupport::UInt32
 
-    #MidiOutCaps() = new(0, 0, 0,        SZPName(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),        0, 0, 0, 0, 0)
     MidiOutCaps() = new(0, 0, 0, ntuple(x -> 0, 32), 0, 0, 0, 0, 0)
 end
 
@@ -76,22 +38,19 @@ function getoutputdevices()
     results
 end
 
-const CALLBACK_NULL = uint32(0x00000000)
 function openoutputdevice(id::UInt32)
     handle = Ref{Cint}(1)
 
     err = ccall((:midiOutOpen, :Winmm), stdcall,
         UInt32,
         (Ref{Cint}, UInt32, Ptr{UInt32}, Ptr{UInt32}, UInt32),
-        Ref{Cint}(handle), id, C_NULL, C_NULL, CALLBACK_NULL)
+        handle, id, C_NULL, C_NULL, CALLBACK_NULL)
 
-    println(hex(err, 4))
-    println(hex(handle[], 4))
-    handle
+    return (UInt32(handle[]), err)
 end
 
 function closeoutputdevice(id::UInt32)
-    handle = uint32(0)
+    handle = UInt32(0)
 
     ccall((:midiOutClose, :Winmm), stdcall,
         UInt32,
